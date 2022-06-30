8286b223fbdec1e6c7f53dd2b0b5fa72186668ad
Michael Dear <michaelcdear@gmail.com>
2022-06-30 12:33:08 -0600

    commit tree 24629dfd8acd6095afc107b506c53d39e18f69bf

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Your code here
+
+const dodger = document.getElementById("dodger")
+dodger.style.backgroundColor = "#FF69B4"
+
+
+
+
+    document.addEventListener("keydown", event => {
+        if (event.key === "ArrowLeft") {
+            moveDodgerLeft()
+        } else if (event.key === "ArrowRight") {
+            moveDodgerRight()
+        }})
+
+    function moveDodgerLeft() {
+        const dodgerLeft = dodger.style.left.replace("px", "")
+            const left = parseInt(dodgerLeft, 10)
+            if (left > 0) {
+            dodger.style.left = `${left - 1}px`
+    }}
+
+function moveDodgerRight() {
+            const dodgerLeft = dodger.style.left.replace("px", "")
+            const left = parseInt(dodgerLeft, 10)
+            if (left < 1000) {
+            dodger.style.left = `${left + 1}px`
+        }}
